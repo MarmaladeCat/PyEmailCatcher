97ecc27d36a44f8bf70e23ed254582d55866d9ba
MarmaladeCat <574702257@qq.com>
2020-09-27 15:53:34 +0800

    .

--- makeMail.py
+++ makeMail.py
@@ -3,7 +3,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.header import Header
-import json
+from poplib import POP3
+import json,re
 import mimetypes
 import email.encoders
 
@@ -11,8 +12,11 @@ import email.encoders
 class MIMEmail:
 
     def __init__(self):
-        self.host = ""
-        self.port = None
+        self.smtp = ""
+        self.pop3 = ""
+        
+        self.port_smtp = None
+        self.port_pop3 = None
         self.authMail = ""
         self.authPasswd = ""
         self.mailFrom = ""
@@ -32,11 +36,15 @@ class MIMEmail:
             Cc.append(addr)
         self.msg["Cc"] = Header(','.join(Cc))
 
-    def setSendinfo(self, sendinfo):
-        self.host = sendinfo["host"]
-        self.port = sendinfo["port"]
-        self.authMail = sendinfo["authMail"]
-        self.authPasswd = sendinfo["authPasswd"]
+    # def setSendinfo(self, sendinfo):
+    #     self.host_smtp = sendinfo["host_smtp"]
+    #     self.host_pop3 = sendinfo["host_pop3"]
+        
+    #     self.port_smtp = sendinfo["port_smtp"]
+    #     self.port_pop3 = sendinfo["port_pop3"]
+
+    #     self.authMail = sendinfo["authMail"]
+    #     self.authPasswd = sendinfo["authPasswd"]
 
     def setContent(self, content):
         part = MIMEMultipart("related")
@@ -74,9 +82,12 @@ class MIMEmail:
         with open(filename, "w+") as fd:
             fd.write(self.msg.as_string())
 
-    def setSendinfo(self, sendinfo):
-        self.host = sendinfo["host"]
-        self.port = sendinfo["port"]
+    def setHostinfo(self, sendinfo):
+        self.host_smtp = sendinfo["host_smtp"]
+        self.host_pop3 = sendinfo["host_pop3"]
+
+        self.port_smtp = sendinfo["port_smtp"]
+        self.port_pop3 = sendinfo["port_pop3"]
         self.authMail = sendinfo["authMail"]
         self.authPasswd = sendinfo["authPasswd"]
         self.mailFrom = sendinfo["fromMail"]
@@ -86,8 +97,12 @@ class MIMEmail:
         self.mailTo = receiver
 
     def peekInfo(self):
-        print("host", self.host)
-        print("port", self.port)
+        print("host_smtp", self.host_smtp)
+        print("host_pop3", self.host_pop3)
+
+        print("port_smtp", self.port_smtp)
+        print("port_pop3", self.port_pop3)
+
         print("authMail", self.authMail)
         print("authPasswd", self.authPasswd)
         print("msg:", self.msg.as_string())
@@ -96,36 +111,75 @@ class MIMEmail:
         with open(jsonfile, "r") as fd:
             MailConfig = json.load(fd)
         MIMEmail.setBasic(self, MailConfig["basic"])
-        MIMEmail.setSendinfo(self, MailConfig["sendinfo"])
+        MIMEmail.setHostinfo(self, MailConfig["sendinfo"])
         MIMEmail.setContent(self, MailConfig["content"])
         MIMEmail.setAttachments(self, MailConfig["attachment"])
 
     def sendBySMTP(self):
         smtp = smtplib.SMTP()
-        smtp.connect(self.host, self.port)
+        smtp.connect(self.host_smtp, self.port_smtp)
         smtp.login(self.authMail, self.authPasswd)
         smtp.sendmail(self.mailFrom, self.mailTo, self.msg.as_string())
         smtp.quit()
+    def getByPOP3(self):
+        pop3server = POP3(self.host_pop3)
+        print(pop3server.getwelcome().decode('utf-8'))
+        # pop3server.set_debuglevel(1)
+        pop3server.user(self.authMail)
+        pop3server.pass_(self.authPasswd)
+        print(pop3server.capa())
+
+        ret = pop3server.stat()
+        print("stat",ret)
+        for i in range(1,ret[0]+1):
+            print('Messages: %s. Size: %s' % tuple(re.findall(r"\d+",pop3server.list(i).decode("UTF-8"))))
+
+        print(pop3server.list())
+
+        pop3server.noop()
+
+        # sleep(3)
+        print(pop3server.uidl())
+
+        for i in range(1,ret[0]+1):
+            print('Messages:{0[1]} uid: {0[2]}'.format(re.findall(r"^\d+|[0-9a-zA-Z\+]+",pop3server.uidl(i).decode("UTF-8"))))
+
+        try:
+            for i in range(1,ret[0]+2):
+                response, lines, octets = pop3server.top(i , 10)
+                print(response,lines,octets)
+        except BaseException as e:
+        else:
+
+        try:
+            for i in range(1,ret[0]+3):
+                response, lines, octets = pop3server.retr(i)
+                print(response,lines,octets)
+        except BaseException as e:
+        else:
+        
+
+
+        pop3server.quit()
+
 
 
 if __name__ == "__main__":
-    with open("mail1.json", "r") as fd:
-        MailConfig = json.load(fd)
-
-    basic = MailConfig["basic"]
-    sendinfo = MailConfig["sendinfo"]
-    content = MailConfig["content"]
-    attachment = MailConfig["attachment"]
-
     oneMail = MIMEmail()
-    oneMail.loadConfigFromJson("./mail1.json")
-    oneMail.peekInfo()
-    oneMail.send2file("./a.txt")
-    oneMail.sendBySMTP()
 
-    # oneMail = MIMEmail()
-    # oneMail.setBasic(basic)
-    # oneMail.setContent(content)
-    # oneMail.setAttachments(attachment)
-    # oneMail.setSendinfo(sendinfo)
+
+    # with open("mail1.json", "r") as fd:
+    #     MailConfig = json.load(fd)
+
+    # basic = MailConfig["basic"]
+    # sendinfo = MailConfig["sendinfo"]
+    # content = MailConfig["content"]
+    # attachment = MailConfig["attachment"]
+
+    oneMail.loadConfigFromJson("./mail1.json")
     # oneMail.peekInfo()
+    oneMail.send2file("./a.txt")
+    # oneMail.sendBySMTP()
+    oneMail.getByPOP3()
+
+
